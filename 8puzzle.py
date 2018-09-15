aba7e63557d0e5f8d756357cd78ae0af68d2d817
zperlo <zperlo@commschool.org>
2018-09-15 13:10:17 -0400

    added maxNodes and the function to calculate h2 goalDist

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,14 +1,17 @@
 from random import randint
+import sys
 
-inputFile = "input.txt"
+#inputFile = "input.txt"
+inputFile = sys.argv[1]
 myInput = open(inputFile, 'r')
 #oneLine = myInput.readline()   #reads one line at a time
 allLines = myInput.readlines()  #reads each line to an array
 
-state = ['b', '1', '2', '3', '4', '5', '6', '7', '8']
+state = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+maxNodes = -1
 
 def move(direction):
-    x = state.index('b')
+    x = state.index('0')
     if direction == "up":
         if x < 3:
             print("Invalid move")
@@ -48,13 +51,31 @@ def move(direction):
 def printState():
     strState = "".join(state)
     strState = strState[:3] + " " + strState[3:6] + " " + strState[6:]
+    strState = strState.replace("0", "b")
     print(strState)
+
+def goalDist(st): #takes in a state as a list
+    count = 0
+    for x in st:
+        if (int(x) < 3 and st.index(x) < 3) or (int(x) >= 3 and int(x) < 6 and st.index(x) >= 3 and st.index(x) < 6) or (int(x) >= 6 and st.index(x) >= 6):
+            count = count + abs((int(x) % 3) - (st.index(x)%3))
+            print("if1 count = " + str(count))
+        elif (int(x) < 3 and st.index(x) >= 3 and st.index(x) < 6) or (int(x) >= 3 and int(x) < 6 and (st.index(x) < 3 or st.index(x) >= 6)) or (int(x) >= 6 and st.index(x) < 6 and st.index(x) >= 3):
+            count = count + abs((int(x) % 3) - (st.index(x)%3)) + 1
+            print("if2 count = " + str(count))
+        else:
+            count = count + abs((int(x) % 3) - (st.index(x)%3)) + 2
+            print("if3 count = " + str(count))
+    return count
+
+#def A-star(n = maxNodes):
 
 for x in allLines:
     if "setState" in x:
         x = x.replace(" ", "")
         x = x.replace("\n", "")
         x = x.replace("setState", "")
+        x = x.replace("b", "0")
         state = list(x)
         print(state)
     elif "move" in x:
@@ -82,5 +103,11 @@ for x in allLines:
                 else:
                     direction = "right"
                 k = move(direction)
+    elif "maxNodes" in x:
+        x = x.replace(" ", "")
+        x = x.replace("\n", "")
+        x = x.replace("maxNodes", "")
+        n = int(x)
+        maxNodes = n
     else:
         print(x)
